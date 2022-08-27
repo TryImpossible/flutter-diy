2b0bf7267633a094c961794818a4f75633ea4e39
xiebin <865068275@qq.com>
2022-08-27 23:52:40 +0800

    isolate的使用

--- app/lib/framework/isolate_compute_page.dart
+++ app/lib/framework/isolate_compute_page.dart
@@ -23,18 +23,34 @@ class _IsolateComputePageState extends State<IsolateComputePage> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const SizedBox(
-              width: 80.0,
-              height: 80.0,
-              child: CircularProgressIndicator(),
+            const Text(
+              '通过复杂计算，测试页面是否会卡顿\n注意loading状态',
+              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 30.0),
-            ElevatedButton(
-              onPressed: _update,
-              child: Text(
-                '点击我更新：$_count',
-                textScaleFactor: 1.5,
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: SizedBox(
+                width: 60.0,
+                height: 60.0,
+                child: CircularProgressIndicator(),
               ),
+            ),
+            Text('当前值：$_count'),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(onPressed: _test1, child: const Text('主线程')),
+                ElevatedButton(onPressed: _test2, child: const Text('compute')),
+                ElevatedButton(
+                  onPressed: _test3,
+                  child: const Text('isolate1'),
+                ),
+                ElevatedButton(
+                  onPressed: _test4,
+                  child: const Text('isolate2'),
+                ),
+              ],
             )
           ],
         ),
@@ -42,28 +58,69 @@ class _IsolateComputePageState extends State<IsolateComputePage> {
     );
   }
 
-  void _update() async {
-    // _count = countEven(1000000000);
-    _count = await compute(countEven, 1000000000);
+  void _test1() {
+    _count = _performCompute(1000000000);
+    setState(() {});
+  }
+
+  void _test2() async {
+    _count = await compute(_performCompute, 2000000000);
     setState(() {});
   }
 
   //计算偶数的个数
-  static int countEven(int num) {
+  static int _performCompute(int num) {
     int count = 0;
     while (num > 0) {
-      if (num % 2 == 0) {
-        count++;
-      }
+      count++;
       num--;
     }
     return count;
   }
 
-  static Future<dynamic> isolateCountEven(int num) async {
-    final response = ReceivePort();
-    await Isolate.spawn((message) {}, response.sendPort);
-    final sendPort = await response.first;
-    final answer = ReceivePort();
+  void _test3() async {
+    final ReceivePort p = ReceivePort();
+    await Isolate.spawn(_fun, [p.sendPort, 3000000000]);
+    _count = await p.first;
+    setState(() {});
+  }
+
+  static void _fun(List<dynamic> args) {
+    SendPort responsePort = args[0];
+    int total = args[1];
+    int count = _performCompute(total);
+    Isolate.exit(responsePort, count);
+  }
+
+  void _test4() async {
+    final ReceivePort p = ReceivePort();
+    Isolate.spawn(_fun2, p.sendPort);
+    p.listen((dynamic message) {
+      if (message is SendPort) {
+        // 接收新的Isolate发送过来的SendPort，并将要将计算的值发送过去
+        final SendPort sendPort = message;
+        sendPort.send(4000000000);
+      } else if (message is int) {
+        // 接收新的Isolate计算结果，并刷新
+        _count = message;
+        setState(() {});
+      }
+    });
+  }
+
+  static void _fun2(SendPort sendPort) {
+    ReceivePort receivePort = ReceivePort();
+    // 新的Isolate把发送器发给主Isolate
+    sendPort.send(receivePort.sendPort);
+
+    receivePort.listen((dynamic message) {
+      if (message is int) {
+        // 接收主Isolate发送过来的数据进行计算
+        int total = message;
+        int count = _performCompute(total);
+        // 将计算结果发送给主Isolate
+        sendPort.send(count);
+      }
+    });
   }
 }
