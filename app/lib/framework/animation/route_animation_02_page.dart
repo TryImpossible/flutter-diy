ccf5ed9b981a8c6ea2e6f1dcd4b1f67dd79e1845
Barry <865068275@qq.com>
2026-04-11 21:45:46 +0800

    [app] feat: 网格路由动画

--- app/lib/framework/animation/route_animation_02_page.dart
+++ app/lib/framework/animation/route_animation_02_page.dart
@@ -16,54 +16,61 @@ class RouteAnimation02Page extends StatelessWidget {
         ),
         itemCount: 30,
         itemBuilder: (_, int index) {
-          return Builder(builder: (BuildContext context) {
-            return InkWell(
-              onTap: () => _onTap(context),
-              child: Hero(
-                tag: index,
-                // flightShuttleBuilder: (
-                //   BuildContext flightContext,
-                //   Animation<double> animation,
-                //   HeroFlightDirection flightDirection,
-                //   BuildContext fromHeroContext,
-                //   BuildContext toHeroContext,
-                // ) {
-                //   return toHeroContext.widget;
-                // },
-                // placeholderBuilder: (
-                //   BuildContext context,
-                //   Size heroSize,
-                //   Widget child,
-                // ) {
-                //   return ColoredBox(color: Colors.green, child: Text('123'));
-                // },
-                child: Container(
-                  alignment: Alignment.center,
-                  color: Colors.primaries[index % Colors.primaries.length],
-                  child: Text(index.toString()),
+          return Builder(
+            builder: (BuildContext context) {
+              final int tag = index;
+              final Color color =
+                  Colors.primaries[index % Colors.primaries.length];
+              return InkWell(
+                onTap: () => _onTap(context, tag: tag, color: color),
+                child: Hero(
+                  tag: tag,
+                  // flightShuttleBuilder: (
+                  //   BuildContext flightContext,
+                  //   Animation<double> animation,
+                  //   HeroFlightDirection flightDirection,
+                  //   BuildContext fromHeroContext,
+                  //   BuildContext toHeroContext,
+                  // ) {
+                  //   return toHeroContext.widget;
+                  // },
+                  // placeholderBuilder: (
+                  //   BuildContext context,
+                  //   Size heroSize,
+                  //   Widget child,
+                  // ) {
+                  //   return ColoredBox(color: Colors.green, child: Text('123'));
+                  // },
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: color,
+                    child: Text(index.toString()),
+                  ),
                 ),
-              ),
-            );
-          });
+              );
+            },
+          );
         },
       ),
     );
   }
 
-  void _onTap(BuildContext context) {
+  void _onTap(BuildContext context, {required int tag, required Color color}) {
     final RenderBox? renderBox = context.findRenderObject() as RenderBox?;
     if (renderBox != null) {
       Offset position = renderBox.localToGlobal(Offset.zero);
       // debugPrint('top left position: $position');
       position += renderBox.size.center(Offset.zero);
       debugPrint(
-          'renderBox position: $position, renderBox size: ${renderBox.size}');
+        'renderBox position: $position, renderBox size: ${renderBox.size}',
+      );
 
       Navigator.of(context).push(
         _CustomPageRoute(
+          parentContext: context,
           position: position,
           builder: (_) {
-            return const _TestPage();
+            return _TestPage(tag: tag, color: color);
           },
         ),
       );
@@ -72,7 +79,10 @@ class RouteAnimation02Page extends StatelessWidget {
 }
 
 class _TestPage extends StatelessWidget {
-  const _TestPage({super.key});
+  const _TestPage({super.key, required this.tag, required this.color});
+
+  final int tag;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
@@ -88,22 +98,23 @@ class _TestPage extends StatelessWidget {
               Navigator.of(context).pop();
             },
             child: Hero(
-              tag: '21',
-              flightShuttleBuilder: (
-                  BuildContext flightContext,
-                  Animation<double> animation,
-                  HeroFlightDirection flightDirection,
-                  BuildContext fromHeroContext,
-                  BuildContext toHeroContext,
-                  ) {
-                return AnimatedBuilder(
-                  animation: animation,
-                  builder: (BuildContext context, Widget? child) {
-                    return FadeTransition(opacity: animation, child: child);
-                  },
-                  child: fromHeroContext.widget,
-                );
-              },
+              tag: 'tag',
+              // flightShuttleBuilder:
+              //     (
+              //       BuildContext flightContext,
+              //       Animation<double> animation,
+              //       HeroFlightDirection flightDirection,
+              //       BuildContext fromHeroContext,
+              //       BuildContext toHeroContext,
+              //     ) {
+              //       return AnimatedBuilder(
+              //         animation: animation,
+              //         builder: (BuildContext context, Widget? child) {
+              //           return FadeTransition(opacity: animation, child: child);
+              //         },
+              //         child: fromHeroContext.widget,
+              //       );
+              //     },
               child: _buildBox(context),
             ),
           ),
@@ -113,11 +124,8 @@ class _TestPage extends StatelessWidget {
   }
 
   Widget _buildBox(BuildContext context) {
-    Widget child = Container(
-      width: 120,
-      height: 120,
-      color: Colors.redAccent,
-    );
+    Widget child = Container(width: 120, height: 120, color: color);
+    return child;
     final Animation<double>? animation = ModalRoute.of(context)?.animation;
     if (animation != null) {
       final Size size = MediaQuery.of(context).size;
@@ -126,7 +134,10 @@ class _TestPage extends StatelessWidget {
         builder: (BuildContext context, Widget? child) {
           return ScaleTransition(
             scale: Tween<double>(begin: 0, end: 1).animate(animation),
-            alignment: FractionalOffset.fromOffsetAndSize(Offset(147.8, 625.6), size),
+            alignment: FractionalOffset.fromOffsetAndSize(
+              Offset(147.8, 625.6),
+              size,
+            ),
             child: child,
           );
         },
@@ -142,6 +153,7 @@ class _CustomPageRoute<T> extends PageRoute<T> {
     super.settings,
     super.fullscreenDialog = false,
     super.allowSnapshotting = true,
+    required this.parentContext,
     required this.position,
     required this.builder,
     this.transitionDuration = const Duration(milliseconds: 800),
@@ -151,6 +163,8 @@ class _CustomPageRoute<T> extends PageRoute<T> {
     this.barrierLabel = '',
     this.maintainState = true,
   });
+
+  final BuildContext parentContext;
 
   final Offset position;
 
@@ -179,20 +193,36 @@ class _CustomPageRoute<T> extends PageRoute<T> {
 
   @override
   Widget buildPage(
-      BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      ) {
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+  ) {
     return builder(context);
   }
 
   @override
   Widget buildTransitions(
-      BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child,
-      ) {
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    final Size windowSize = MediaQuery.of(parentContext).size;
+    final RenderBox renderBox = parentContext.findRenderObject() as RenderBox;
+    final Rect rect = renderBox.localToGlobal(Offset.zero) & renderBox.size;
+    final RelativeRect relativeRect = RelativeRect.fromSize(rect, windowSize);
+    final RelativeRectTween relativeRectTween = RelativeRectTween(
+      begin: relativeRect,
+      end: RelativeRect.fill,
+    );
+    final Animation<RelativeRect> rectAnimation = relativeRectTween
+        .chain(CurveTween(curve: Curves.ease))
+        .animate(animation);
+
+    return Stack(
+      children: [PositionedTransition(rect: rectAnimation, child: child)],
+    );
+
     if (animation.status == AnimationStatus.forward) {
       return child;
     } else {
@@ -204,22 +234,30 @@ class _CustomPageRoute<T> extends PageRoute<T> {
           return Stack(
             children: <Widget>[
               Transform.translate(
-                offset: Tween(begin: Offset.zero, end: Offset.zero)
-                    .evaluate(animation),
+                offset: Tween(
+                  begin: Offset.zero,
+                  end: Offset.zero,
+                ).evaluate(animation),
                 child: Transform.scale(
                   alignment: FractionalOffset.fromOffsetAndSize(position, size),
-                  scale:
-                  Tween<double>(begin: 0.4, end: 1.0).evaluate(animation),
+                  scale: Tween<double>(
+                    begin: 0.4,
+                    end: 1.0,
+                  ).evaluate(animation),
                   child: FadeTransition(
-                    opacity:
-                    Tween<double>(begin: 0.0, end: 1.0).animate(animation),
+                    opacity: Tween<double>(
+                      begin: 0.0,
+                      end: 1.0,
+                    ).animate(animation),
                     child: Container(
                       clipBehavior: Clip.hardEdge,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(
                           Radius.circular(
-                            Tween<double>(begin: 0, end: 72)
-                                .evaluate(animation),
+                            Tween<double>(
+                              begin: 0,
+                              end: 72,
+                            ).evaluate(animation),
                           ),
                         ),
                       ),
